095f2c35c3ec2caf55868ca79acdf19d1077067b
dhruvi-patel24 <patel24dhruvi@gmail.com>
2025-12-23 14:54:44 +0530

    Removed devise recoverable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :rememberable, :validatable
 
   enum :role, { admin: 0, school_admin: 1, student: 2 }, default: :student
 
